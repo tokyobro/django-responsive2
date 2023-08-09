9a25d8403a5ea517a66af3e346ce38b07df44023
tokyobro <fjksct@gmail.com>
2023-08-10 00:24:48 +0900

    Update conf.py
    Django 4. ugettext_lazy has been removed in Django 4.0 628. use gettext_lazy instead:

--- responsive/conf.py
+++ responsive/conf.py
@@ -1,5 +1,6 @@
 from django.conf import settings  # noqa
-from django.utils.translation import ugettext_lazy as _
+# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from appconf import AppConf
 
